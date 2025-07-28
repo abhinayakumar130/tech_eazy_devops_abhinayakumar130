c7d12a9713837610977130315f2a12f5fdcbb2d3
abhinaya kumar <abhinayakuma@gmail.com>
2025-07-29 00:28:05 +0530

    Updated test_script.sh

--- terraform/test_script.sh
+++ terraform/test_script.sh
@@ -16,10 +16,10 @@ sudo chown ubuntu:ubuntu /opt/tech_eazy_devops_abhinayakumar130/app.log
 echo "ERROR: Something went wrong in test instance!" >> /opt/tech_eazy_devops_abhinayakumar130/app.log
 
 # Download CloudWatch config
-wget https://raw.githubusercontent.com/abhinayakumar130/tech_eazy_devops_abhinayakumar130/ass-5/terraform/cloudwatch-config.json -O /opt/cloudwatch-config.json
+sudo wget https://raw.githubusercontent.com/abhinayakumar130/tech_eazy_devops_abhinayakumar130/ass-5/terraform/cloudwatch-config.json -O /opt/cloudwatch-config.json
 
 # Start the CloudWatch Agent
-/opt/aws/amazon-cloudwatch-agent/bin/amazon-cloudwatch-agent-ctl \
+sudo /opt/aws/amazon-cloudwatch-agent/bin/amazon-cloudwatch-agent-ctl \
   -a fetch-config -m ec2 \
   -c file:/opt/cloudwatch-config.json -s
 
